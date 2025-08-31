7965c35c60c8e2c5b2aa1a87db027ff4524aa41b
Ömer Yiğit Tatlisu <93652315+omery-t@users.noreply.github.com>
2025-08-31 16:25:26 +0300

    Base react structure set up

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home, NotFound } from "@/pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes> 
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
